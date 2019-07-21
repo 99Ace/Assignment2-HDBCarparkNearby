23f969a3cc88d44d17adfaadaf6576b7bcebfc2d
Ubuntu <ubuntu@ip-172-31-81-217.ec2.internal>
2019-07-21 16:35:41 +0000

    added to detect click and show current location

--- scripts/script.js
+++ scripts/script.js
@@ -6,11 +6,9 @@ var locationSearch = {
   fields: ['name', 'geometry'],
 }; //for holding the data to set the map
 var markers = [];
-var service;
 
-// var searchBox = new google.m/aps.places.SearchBox(input);
-
-//Initiate google map to be displayed
+//GOOGLE MAP FUNCTIONS
+//DISPLAY MAP
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 1.3521, lng: 103.8198 },
@@ -21,7 +19,7 @@ function initMap() {
   getCurrentLocation();
   
 }
-//Shows the current location of user else return error
+//FUNCTION TO GET THE LOCATION OF USER
 function getCurrentLocation(){
   if (navigator.geolocation) {
     //get the location of the user current position
@@ -50,9 +48,7 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     'Error: Your browser doesn\'t support geolocation.');
   infoWindow.open(map);
 }
-
-//function to place marker of coordinates given
-//myLatLng to set location, map to set map to place marker
+//CREATE MARKER FUNCTION FOR CURRENT LOCATION
 function markerPlacement(myLatLng, map) { 
   var marker = new google.maps.Marker({
     position: myLatLng,
@@ -62,46 +58,121 @@ function markerPlacement(myLatLng, map) {
   markers.push(marker);
 }
 function createMarker(place,map) {
-  clearMarker();
   var marker = new google.maps.Marker({
     position: place.geometry.location,
     map: map,
   });
-  // Gives info about location when click upon
+  // SHOWS THE LOCATION DETAIL WHEN CLICK UPON
   google.maps.event.addListener(marker, 'click', function() {
     infowindow.setContent(place.name);
     infowindow.open(map, this);
   });
   markers.push(marker);
 }
+//FUNCTION TO REMOVE MARKERS
 function clearMarker(){
   markers.forEach(function(marker) {
     marker.setMap(null);
   });
   markers = [];
 }
-
-//Search for location and place a marker on the map and center to the location
+//FUNCTION TO SEARCH FOR A LOCATION AND PLACE A MARKER
 function searchAndPlace(locationSearch){
+    clearMarker();
     service.findPlaceFromQuery(locationSearch, function(results, status) {
       if (status === google.maps.places.PlacesServiceStatus.OK) {
         for (var i = 0; i < results.length; i++) {
           createMarker(results[i],map);
-          // console.log(results[i]);
-          // markerPlacement(results[i], map);
         }
         map.setCenter(results[0].geometry.location);
-        // console.log (results[0].geometry.location)
       }
     });
 }
 
+var dataSource = "https://api.data.gov.sg/v1/transport/carpark-availability";
+var dataSource2 = "carparkdata.json";
+var carparkData = []
 
+//AXIO TO GET DATA FROM API
+function getDataFromEndpoint(callback) {
+  axios.get(dataSource)
+    .then(function(response) {
+      let result = response.data.items[0].carpark_data;
+      // // console.log(result)
+      callback(result)
+    })
+}
+//AXIO TO GET DATA FROM JSON FILE (local)
+function getDataFromFile(callback) {
+  axios.get(dataSource2)
+    .then(function(response) {
+      let result = response.data;
+      // console.log(result)
+      callback(result) 
+    })
+}
+//HEVERSINE FORMULA TO CALCULATE DISTANCE BETWEEN 2 LATLNG (CREDIT to stackOverflow solution)
+function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2 - lat1); // deg2rad below
+  var dLon = deg2rad(lon2 - lon1);
+  var a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+    Math.sin(dLon / 2) * Math.sin(dLon / 2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  var d = R * c; // Distance in km
+  return d;
+}
+
+
+// alert (getDistanceFromLatLonInKm(1.3521, 103.8198, 1.3531,103.8198 ))
+
+
+//FUNCTION TO MERGE DATA FROM API AND JSON FILE
+getDataFromFile(function(carparkAddInfo){
+  getDataFromEndpoint(function(data){
+    for (let item in data) {
+      carparkData[item] = {
+        "carpark_number": data[item].carpark_number,
+        "lot_type": data[item].carpark_info[0]["lot_type"],
+        "lots_available": data[item].carpark_info[0]["lots_available"],
+        "total_lots": data[item].carpark_info[0]["total_lots"]
+      };
+      for (let item2 in carparkAddInfo) {
+        if (carparkAddInfo[item2].car_park_no == data[item].carpark_number) {
+          Object.assign(carparkData[item], {
+            "address": carparkAddInfo[item2].address,
+            "car_park_basement": carparkAddInfo[item].car_park_basement,
+            "car_park_decks": carparkAddInfo[item].car_park_decks,
+            "car_park_type": carparkAddInfo[item].car_park_type,
+            "free_parking": carparkAddInfo[item].free_parking,
+            "gantry_height": carparkAddInfo[item].gantry_height,
+            "night_parking": carparkAddInfo[item].night_parking,
+            "short_term_parking": carparkAddInfo[item].short_term_parking,
+            "type_of_parking_system": carparkAddInfo[item].type_of_parking_system,
+            "x_coord": carparkAddInfo[item].x_coord,
+            "y_coord": carparkAddInfo[item].y_coord
+          })
+
+        }
+      } //Working Set : Do not change
+    }
+  })
+})
+
+
+  
+// console.log(carparkData)
 $(function() {
   // Get the input location from user; Set to search within Singapore Only
   $("#location-submit").click(function() {
     // marker.setMap(null);
     locationSearch.query = ($("#search").val()) + " Singapore"; 
     searchAndPlace(locationSearch);
-  })
+  });
+  $('#current-location').click(function(){
+    clearMarker();
+    getCurrentLocation();
+  });
 })
